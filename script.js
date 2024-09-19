9828bd98d6a313c49ba6cd0604a8a556a95ffb15
Felipe <143225058+Felipe572@users.noreply.github.com>
2024-09-19 18:11:21 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -1,56 +1,28 @@
-let av1 = []
-let av2 = []
-let av3 = []
-
 const studants = [
-    {
-        name: "Pedro",
-        av1: 10,
-        av2: 8,
-        av3: 10
-    },
-    {
-        name: "Marcos",
-        av1: 8,
-        av2: 7,
-        av3: 7
-    },
-    {
-        name: "Maria",
-        av1: 10,
-        av2: 10,
-        av3: 10
-    },
-    {
-        name: "João",
-        av1: 5,
-        av2: 5,
-        av3: 6
-    },
-    {
-        name: "Clara",
-        av1: 7,
-        av2: 8,
-        av3: 8
-    },
-]
+  { name: "João", av1: 8, av2: 6, av3: 10 },
+  { name: "Maria", av1: 7, av2: 9, av3: 7 },
+  { name: "Pedro", av1: 5, av2: 4, av3: 5 },
+  { name: "Ana", av1: 10, av2: 9, av3: 10 }
+];
 
 function average (av1, av2, av3) {
-    return ((studants.av1 + studants.av2 + studants.av3) / 3 ).toFixed(2)
+    return ((av1 + av2 + av3) / 3).toFixed(2)
 }
 
-function printStudants(student) {
-    return
+function approvedStudent(studant) {
+    const media = average(studant.av1, studant.av2, studant.av3);
+
+    if (media >= 7) {
+        alert(`
+            A média do(a) aluno(a) ${studant.name} é: ${media}\n
+            Parabén, ${studant.name}! Você foi aprovado(a) no curso
+            `)
+    } else {
+        alert(`
+            A média do(a) aluno(a) ${studant.name} é: ${media}\n
+            Não foi dessa vez, ${studant.name}! Tentar novamente!
+            `)
+    }
 }
 
-if (average >= 7) {
-    alert(`
-        A média do(a) aluno(a) ${studant.name} é: ${average}\n
-        Parabén, ${studant.name}! Você foi aprovado(a) no curso
-        `)
-} else {
-    alert(`
-        A média do(a) aluno(a) ${student.name} é: ${average}\n
-        Não foi dessa vez, ${student.name}! Tentar novamente!
-        `)
-}
+studants.forEach(approvedStudent)
